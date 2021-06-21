4ef5f7077816f5d6d75b22134d246c7c6d6d1d6e
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-06-21 14:57:26 +0800

    upgrade kernel to 5.10

--- onekey/compile.sh
+++ onekey/compile.sh
@@ -82,18 +82,19 @@ case $CHOOSE in
 esac
 done
 
+
+
 git clone -b openwrt-21.02 --depth 1 https://github.com/openwrt/openwrt
 svn co https://github.com/garypang13/OpenWrt/trunk/devices openwrt/devices
 
+cd openwrt
+
 if [[ $firmware =~ (nanopi-r2s|nanopi-r4s) ]]; then
-		cd openwrt
-		wget -cO sdk.tar.xz https://mirrors.cloud.tencent.com/openwrt/releases/21.02-SNAPSHOT/targets/rockchip/armv8/openwrt-sdk-21.02-SNAPSHOT-rockchip-armv8_gcc-8.4.0_musl.Linux-x86_64.tar.xz
+		wget -cO sdk.tar.xz https://mirrors.cloud.tencent.com/openwrt/snapshots/targets/rockchip/armv8/openwrt-sdk-rockchip-armv8_gcc-8.4.0_musl.Linux-x86_64.tar.xz
 elif [[ $firmware == "x86_64" ]]; then
-		cd openwrt
-		wget -cO sdk.tar.xz https://mirrors.cloud.tencent.com/openwrt/releases/21.02-SNAPSHOT/targets/x86/64/openwrt-sdk-21.02-SNAPSHOT-x86-64_gcc-8.4.0_musl.Linux-x86_64.tar.xz
+		wget -cO sdk.tar.xz https://mirrors.cloud.tencent.com/openwrt/snapshots/targets/x86/64/openwrt-sdk-x86-64_gcc-8.4.0_musl.Linux-x86_64.tar.xz
 elif [[ $firmware == "Rpi-4B" ]]; then
-		cd openwrt
-		wget -cO sdk.tar.xz https://mirrors.cloud.tencent.com/openwrt/releases/21.02-SNAPSHOT/targets/bcm27xx/bcm2711/openwrt-sdk-21.02-SNAPSHOT-bcm27xx-bcm2711_gcc-8.4.0_musl.Linux-x86_64.tar.xz
+		wget -cO sdk.tar.xz https://mirrors.cloud.tencent.com/openwrt/snapshots/targets/bcm27xx/bcm2711/openwrt-sdk-bcm27xx-bcm2711_gcc-8.4.0_musl.Linux-x86_64.tar.xz
 fi
 
 
